fa2559fc0f29b1f9c61ac6c8d22bb83da1231ace
Mahfuzislam1695 <mahfuzislam1695@gmail.com>
2022-04-08 12:13:58 +0600

    finish

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors())
+app.use(express.json());
 // const handler = (req, res) => {
 //     res.send('Hello from node')
 // }
 
 //app.get('/',handler);
+
+app.get('/', (req, res) => {
+    res.send('Hello from node')
+})
 
 const users =[
     { id: 0, name: 'John', email: 'jon@gmail.com', phone: '123-456-1234' },
@@ -16,12 +23,24 @@ const users =[
     { id: 4, name: 'lemon', email: 'lemon@gmail.com', phone: '123-456-1234' },
 ]
 
-app.get('/', (req, res) => {
-    res.send('Hello from node')
-})
 
 app.get('/users', (req, res) => {
-    res.send(users)
+    const search = req.query.search;
+    if (search) {
+        const searchResult = users.filter(user => user.name.toLocaleLowerCase().includes(search));
+        res.send(searchResult);
+    }
+    else {
+        res.send(users)
+    }
+});
+
+app.post("/users", (req, res) => {
+    const newUser = req.body;
+    newUser.id = users.length;
+    users.push(newUser);
+    console.log('hitting the post', req.body);
+    res.json(newUser)
 })
 
 app.get('/users/:id', (req, res) => {
