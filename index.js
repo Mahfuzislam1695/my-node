908cb589c77c9f909469ebf8d2bc5b31ec968cf6
Mahfuzislam1695 <mahfuzislam1695@gmail.com>
2022-04-14 12:12:29 +0600

    node-final

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express');
-var cors = require('cors')
+var cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json());
-// const handler = (req, res) => {
-//     res.send('Hello from node')
-// }
 
-//app.get('/',handler);
 
 app.get('/', (req, res) => {
     res.send('Hello from node')
@@ -25,7 +21,9 @@ const users =[
 
 
 app.get('/users', (req, res) => {
+ //   console.log(req.query);
     const search = req.query.search;
+    // use query parameter
     if (search) {
         const searchResult = users.filter(user => user.name.toLocaleLowerCase().includes(search));
         res.send(searchResult);
@@ -35,16 +33,25 @@ app.get('/users', (req, res) => {
     }
 });
 
+app.get('/fruits', (req, res) => {
+    res.send(['mango','apple','orange'])
+})
+
+//app.Method
+
 app.post("/users", (req, res) => {
     const newUser = req.body;
     newUser.id = users.length;
     users.push(newUser);
     console.log('hitting the post', req.body);
+    // res.send(JSON.stringify(newUser));
     res.json(newUser)
 })
 
+// dynamic api
 app.get('/users/:id', (req, res) => {
-    const id = req.params.id;
+//    console.log(req.params.id);
+   const id = req.params.id;
     const user = users[id]
     res.send(user);
 })
